69fbb17978563f3aefa56f05128f872def04dc7b
matckan <148798315+matckan@users.noreply.github.com>
2024-01-13 00:31:44 +0300

    Add files via upload

---
+++ cypress/e2e/mytest.cy.js
@@ -0,0 +1,78 @@
+
+describe('Автотесты на login.qa.studio', function () {
+     it('Верный логин и пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        })
+
+
+        it('Логика восстановления пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('mail777@mail.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon');
+        })
+
+        it('Негативный кейс авторизации (пароль)', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('wrongpass');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon');
+        })
+
+        it('Негативный кейс авторизации (логин)', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('wrong@mail.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon');
+        })
+
+        it('Негативный кейс валидации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Нужно исправить проблему валидации');
+        })
+
+        it('Приведение к строчным буквам в логине', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon');
+        })
+})
+
+
+describe('Автотесты на pokemonbattle.me', function () {
+        it('Покупка фото для тренера', function () {
+        cy.visit('https://pokemonbattle.me/login');
+        cy.get('#root > div > main > section > div.login__content > form > div:nth-child(1) > input').type('matckan@yandex.ru');
+        cy.get('#password').type('Testqa9');
+        cy.get('.auth__button').click();
+        cy.get('.header__btns > [href="/shop"]').click();
+        cy.get('.shop__list > li').not('.feature-empty').children('.shop__button').eq(0).click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('411111111111   1111');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1224');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('Test Payment');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.contains('Покупка прошла успешно').should('be.visible');
+        cy.get('.payment__adv').click();
+        })
+
+})
